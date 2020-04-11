69d916b732a8451ff2e8a8c46ba72e58156b3495
Erdem Bektaş <actasalways@gmail.com>
2020-04-11 21:59:02 +0300

    protect-the-spaceship

---
+++ save-the-spaceship/Assets/DuslamlarinCiktigiYer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuslamlarinCiktigiYer : MonoBehaviour {
+
+    public GameObject dusmanPrefabi;
+
+	// Use this for initialization
+	void Start () {
+        foreach(Transform cocuk in transform)
+        {
+            GameObject dusman = Instantiate(dusmanPrefabi, cocuk.transform.position, Quaternion.identity) as GameObject;
+            dusman.transform.parent = cocuk;
+
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
